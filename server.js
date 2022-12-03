31ed4e47db9b98138be89dc3ff372eac4a3a70f2
ParumaUna <una.paruma10@gmail.com>
2022-12-03 20:47:47 +0100

    first contact & 1:st subdirectory made

--- server.js
+++ server.js
@@ -5,3 +5,20 @@ app.use(express.json())
 app.listen(8000,() => {
     console.log('server is working');
 })
+
+var contacts = [
+    {
+        id:"1",
+        name:"Joe"
+    }
+]
+
+app.get('/contact',(req,res) => {
+    res.send({
+        success:true,
+        message:'Data is saved successfully',
+        data:contacts
+    })
+})
+
+
