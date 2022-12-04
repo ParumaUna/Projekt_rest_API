a60116f8fb3c64917f68948476e953e1fe9f651f
ParumaUna <una.paruma10@gmail.com>
2022-12-04 23:49:20 +0100

    if sats för updateUser

--- controllers/UserControllers.js
+++ controllers/UserControllers.js
@@ -41,15 +41,23 @@ export const updateUser = (req,res) => {
     var id = req.params.id
     var name = req.body.name
     
-    var index = contacts.findIndex(el => el.id == id)
+    if(name){
+         var index = contacts.findIndex(el => el.id == id)
     
-    contacts[index] = {
-        ...contacts[index],
-        name:name
+        contacts[index] = {
+            ...contacts[index],
+            name:name
+        }
+    
+        res.send({
+            success:true,
+            message:'Data changed successfully',
+        })
+    }else{
+        res.send({
+            success:false,
+            message:"Cannot be null",
+        })
     }
-
-    res.send({
-        success:true,
-        message:'Data changed successfully',
-    })
+   
 }
